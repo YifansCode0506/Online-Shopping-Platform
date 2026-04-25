f8b06e1d04d3ab5fd58fc08c0f8358874a936ef5
YifansCode0506 <yifan.ky0523@outlook.com>
2026-04-26 01:22:56 +0930

    elementPluse

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,27 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+//element plus
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    // ...
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
   ],
   resolve: {
+    // actually pathway trans @ -> src
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
