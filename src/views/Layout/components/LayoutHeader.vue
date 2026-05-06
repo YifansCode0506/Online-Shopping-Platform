7858cfbabc3d71bcfba81bc36c55faa90ed77e29
YifansCode0506 <yifan.ky0523@outlook.com>
2026-05-06 19:16:37 +0930

    Nav links and breadcrumb config

--- src/views/Layout/components/LayoutHeader.vue
+++ src/views/Layout/components/LayoutHeader.vue
@@ -26,7 +26,7 @@
       </h1>
       <ul class="app-header-nav">
         <li class="home" v-for = "item in categoryStore.categoryList" :key = "item.id">
-          <RouterLink to="/">{{ item.name }}</RouterLink>
+          <RouterLink :to="`/category/${item.id}`">{{ item.name }}</RouterLink>
         </li>
         <!-- <li> <RouterLink to="/">居家</RouterLink> </li>
         <li> <RouterLink to="/">美食</RouterLink> </li>
