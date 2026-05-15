698bb317c62319263278c1c970e21e126dd7dc38
YifansCode0506 <yifan.ky0523@outlook.com>
2026-05-15 18:09:43 +0930

    login - form validate - Unified verification

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,39 +1,60 @@
 <script setup>
-import { ref } from 'vue';
+    import { ref } from 'vue';
 
 // 表单校验（账户名+密码）
 
 // 1.准备表单对象
-const form = ref({
-    account:'',
-    password:'',
-    agree: true,
-})
+    const form = ref({
+        account:'',
+        password:'',
+        agree: true,
+    })
 
 // 2.规则对象
-const rules ={
-    account:[
-        { required: true, message: '用户名不能为空', trigger:'blur'}
-    ],
-    password:[
-        {required: true, message: '密码不能为空', trigger:'blur'},
-        {min: 6, max: 24, message: '密码长度要求6-14个字符', trigger:'blur'},
-    ],
-    agree:[
-        {
-            validator: (rule, value, callback) =>{
-                // console.log(value);
-                // 自定义校验逻辑
-                // 勾选就通过，不勾选就不通过
-                if(value){
-                    callback()
-                }else{
-                    callback(new Error('请勾选协议'))
+    const rules ={
+        account:[
+            { required: true, message: '用户名不能为空', trigger:'blur'}
+        ],
+        password:[
+            {required: true, message: '密码不能为空', trigger:'blur'},
+            {min: 6, max: 24, message: '密码长度要求6-14个字符', trigger:'blur'},
+        ],
+        agree:[
+            {
+                validator: (rule, value, callback) =>{
+                    // console.log(value);
+                    // 自定义校验逻辑
+                    // 勾选就通过，不勾选就不通过
+                    if(value){
+                        callback()
+                    }else{
+                        callback(new Error('请勾选协议'))
+                    }
                 }
             }
-        }
-    ]
-}
+        ]
+    }
+
+// 3.获取form实例做统一校验
+    const formRef = ref(null);
+    const doLogin = ()=>{
+        // 调用实例方法
+        formRef.value.validate((valid)=>{
+            // valid 所有表单都通过校验 才为true
+            console.log(valid);
+            // 以valid做为判断条件 如果通过校验才执行登录逻辑
+            if(valid){
+                // TODO LOGIN
+            }
+            
+        })
+    }
+
+    // 1.用户名和密码: 只需要通过简单的配置(看文档的方式- 复杂功能通过多个不同组件拆解)
+    // 2.同意协议:   自定义规则 validator:(rule,value,callback)=>[}
+    // 3.统一校验:   通过调用form实例的方法 validate ->true
+
+
 </script>
 
 
@@ -58,7 +79,7 @@ const rules ={
         </nav>
         <div class="account-box">
           <div class="form">
-            <el-form :model="form" :rules="rules" label-position="right" label-width="60px"
+            <el-form ref="formRef" :model="form" :rules="rules" label-position="right" label-width="60px"
               status-icon>
               <el-form-item prop="account" label="账户">
                 <el-input v-model="form.account"/>
@@ -71,7 +92,7 @@ const rules ={
                   我已同意隐私条款和服务条款
                 </el-checkbox>
               </el-form-item>
-              <el-button size="large" class="subBtn">点击登录</el-button>
+              <el-button size="large" class="subBtn" @click="doLogin">点击登录</el-button>
             </el-form>
           </div>
         </div>
