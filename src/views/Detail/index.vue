9c9b6ac85094efc37ec082b86260ba5ef46236ef
YifansCode0506 <yifan.ky0523@outlook.com>
2026-05-15 12:16:58 +0930

    details-basic components and bata rendering

--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -1,4 +1,5 @@
 <script setup>
+  import DetailHot from './components/DetailHot.vue';
   import { getDetail } from '@/apis/detail'
   import { onMounted, ref } from 'vue';
   import { useRoute } from 'vue-router';
@@ -120,7 +121,10 @@
             </div>
             <!-- 24热榜+专题推荐 -->
             <div class="goods-aside">
-
+              <!-- 24 hourse -->
+              <DetailHot />
+              <!-- weekly -->
+              <DetailHot />
             </div>
           </div>
         </div>
