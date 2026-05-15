4a2955d36f69e93b516210526c9c197138783f12
YifansCode0506 <yifan.ky0523@outlook.com>
2026-05-15 13:46:49 +0930

    picture review - props adaptation and optimize

--- src/components/ImageView/index.vue
+++ src/components/ImageView/index.vue
@@ -2,23 +2,31 @@
     import { ref, watch } from 'vue';
     import { useMouseInElement } from '@vueuse/core'
 
-// 图片列表
-    const imageList = [
-    "https://yanxuan-item.nosdn.127.net/d917c92e663c5ed0bb577c7ded73e4ec.png",
-    "https://yanxuan-item.nosdn.127.net/e801b9572f0b0c02a52952b01adab967.jpg",
-    "https://yanxuan-item.nosdn.127.net/b52c447ad472d51adbdde1a83f550ac2.jpg",
-    "https://yanxuan-item.nosdn.127.net/f93243224dc37674dfca5874fe089c60.jpg",
-    "https://yanxuan-item.nosdn.127.net/f881cfe7de9a576aaeea6ee0d1d24823.jpg"
-    ]
+//  props 适配图片列表
+    defineProps({
+        imageList: {
+            type: Array,
+            default: ()=>[]
+        }
+    })
 
-//1.小图切换大图显示
+// 图片列表
+    // const imageList = [
+    // "https://yanxuan-item.nosdn.127.net/d917c92e663c5ed0bb577c7ded73e4ec.png",
+    // "https://yanxuan-item.nosdn.127.net/e801b9572f0b0c02a52952b01adab967.jpg",
+    // "https://yanxuan-item.nosdn.127.net/b52c447ad472d51adbdde1a83f550ac2.jpg",
+    // "https://yanxuan-item.nosdn.127.net/f93243224dc37674dfca5874fe089c60.jpg",
+    // "https://yanxuan-item.nosdn.127.net/f881cfe7de9a576aaeea6ee0d1d24823.jpg"
+    // ]
+
+//  1.小图切换大图显示
     const activeIndex = ref(0);
 
     const enterhandler = (i)=>{
         activeIndex.value = i;
     }
 
-// 2. 获取鼠标相对位置
+//  2. 获取鼠标相对位置
     const target = ref(null);
     const { elementX, elementY, isOutside } = useMouseInElement(target);
 
@@ -33,7 +41,7 @@
         // console.log('xy changes');
         // 如果鼠标没有移入到盒子里面 直接不执行后面的逻辑
         if (isOutside.value) return
-        console.log('后续逻辑执行了')
+        // console.log('后续逻辑执行了')
         // 有效范围内控制滑块距离
         // 横向
         if (elementX.value > 100 && elementX.value < 300) {
