5d62543b7b5cde209f81d9ef4ebea577900613bc
YifansCode0506 <yifan.ky0523@outlook.com>
2026-05-17 15:32:46 +0930

    Carried in the token interceptor

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,6 +1,7 @@
 // Basic Axios encapsulation
 import axios from "axios";
 import { ElMessage } from 'element-plus'
+import { useUserStore } from "@/stores/user";
 import 'element-plus/theme-chalk/el-message.css'
 
 const httpInstance = axios.create({
@@ -11,6 +12,14 @@ const httpInstance = axios.create({
 
 // Axios request interceptor
 httpInstance.interceptors.request.use(config => {
+  // 1.从pinia获取token数据
+  const userStore = useUserStore();
+  // 2.按照后端的要求拼接数据
+const token = userStore.userInfo.token
+if(token){
+  config.headers.Authorization = `Bearer ${token}`
+}
+
   return config;
 }, e => Promise.reject(e));
 
